09e528584aef53ad5bee67f302994b6f6dee0d63
flutter_gmaps <mantas.pan1@gmail.com>
2020-09-13 19:13:15 +0300

    structural changes added trailing and my devises selections

--- lib/screens/myDevices.dart
+++ lib/screens/myDevices.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
-
+import 'package:flutter_picker/Picker.dart';
+import 'package:keyless2/PickerData.dart';
+import 'package:keyless2/pickervalue.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+
+import '../PickerMeterData.dart';
 class myDevices extends StatefulWidget {
   @override
   _myDevicesState createState() => _myDevicesState();
@@ -13,12 +18,17 @@ class _myDevicesState extends State<myDevices> {
   FlutterBlue flutterBlue = FlutterBlue.instance;
   StreamSubscription<ScanResult> scanSubScription;
   List<String> deviceName;
+  bool _visible = true;
   Future<List<String>> deviceName2;
+  var monVal= true;
+  final GlobalKey<ScaffoldState> _scaffoldKey1 = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
+    var checkedValue=false;
     return Scaffold(
-      appBar: AppBar(
+      key: _scaffoldKey1,
+        appBar: AppBar(
         title: Text("my Devices"),
         backgroundColor: Color(0xffB8141F),
       ),
@@ -28,17 +38,81 @@ class _myDevicesState extends State<myDevices> {
             builder: (c, snapshot) => Column(
               children: snapshot.data
                   .map(
-                    (r) => Card(
-                      child: ListTile(
-                        title: new Center(
-                            child: new Text(
-                              r.device.name,
-                              style: new TextStyle(
-                                  fontWeight: FontWeight.w500, fontSize: 15.0),
-                            )),
-                        onTap: () {
-                          print("skata");
-                        },
+                    (r) => Container(
+                      height: 100,
+                      child: Card(
+                        child: ListTile(
+                          title: new Center(
+                              child: new Text(
+                                r.device.name,
+                                style: new TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 15.0),
+                              )),
+                          onTap: () {
+                            print("skata");
+                            showPicker(context);
+                            setState(() {
+                              _visible = false;
+                            });
+                          },
+                          trailing: Row(
+
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Column(
+                                children: [
+                                  Flexible(child: Text("Add")),
+                                  Flexible(
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.add,
+                                        size: 30.0,
+                                        color: Colors.brown[900],
+                                      ),
+                                      onPressed: () {
+                                        print("add");
+                                        //   _onDeleteItemPressed(index);
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Flexible(child: Text("Delete")),
+                                  Flexible(
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.delete_outline,
+                                        size: 30.0,
+                                        color: Colors.brown[900],
+                                      ),
+                                      onPressed: () {
+                                        print("delete");
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            Column(
+                              children: [
+                                Flexible(child: Text("Approaching")),
+                                SizedBox(height: 10,),
+                                Flexible(
+                                  child: Checkbox(
+                                    value: monVal,
+                                    onChanged: (bool value){
+                                      setState(() {
+                                        monVal = value;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            )
+                            ],
+                          ),
+                        ),
                       ),
                     ),
               )
@@ -59,7 +133,7 @@ class _myDevicesState extends State<myDevices> {
             return Container(
 
                 child: Visibility(
-                  visible: true,
+                  visible: _visible,
                   child: FloatingActionButton(
                       backgroundColor: Color(0xffB8141F),
                       child: Icon(Icons.search,
@@ -74,4 +148,24 @@ class _myDevicesState extends State<myDevices> {
       ),
           );
   }
+
+  showPicker(BuildContext context) {
+
+    Picker picker = Picker(
+        adapter: PickerDataAdapter<String>(pickerdata: JsonDecoder().convert(PickerMeterData)),
+        changeToFirst: false,
+        textAlign: TextAlign.left,
+        textStyle: const TextStyle(color: Colors.blue , fontSize: 17.0),
+        selectedTextStyle: TextStyle(color: Colors.red, fontSize: 17.0),
+        columnPadding: const EdgeInsets.all(0.0),
+        onConfirm: (Picker picker, List value) {
+          setState(() {
+            _visible = true;
+          });
+          print(value[1].toString());
+        }
+    );
+    picker.show(_scaffoldKey1.currentState);
+  }
+
 }
